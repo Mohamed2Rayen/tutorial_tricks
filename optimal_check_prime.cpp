c1784343789a92bbd62f870532af8852728d434c
MohamedRayen.srasra <rayensrasra20@gmail.com>
2025-10-11 21:18:04 +0100

    Create optimal_check_prime.cpp
    add prime optimal solution file

---
+++ optimal_check_prime.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fin  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+using namespace std;
+int main(){
+    fin;
+    ll cpt=0 , n;
+    cin>>n;
+    for (int i=1 ; n>=i*i ; i++){ // to check only for √n (i*i<=n) == i<=√n
+        if (n%i == 0){
+            cpt++;
+            if (n/i != i){ // count only the divisor and its pair , see the second file for explanation  
+                cpt++;
+            }
+            if (n>2){
+                break;
+            }
+        }
+    }
+    if (cpt == 2){
+        cout<<"YES";
+    }
+    else {
+        cout<<"NO";
+    }
+    return 0;
+}
